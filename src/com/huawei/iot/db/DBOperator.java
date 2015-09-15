9f47bb6b3386705154e66881c3c340f6ae083812
sylar <sheofir@gmail.com>
2015-09-15 16:07:51 +0800

    增加删除设备数据接口

--- src/com/huawei/iot/db/DBOperator.java
+++ src/com/huawei/iot/db/DBOperator.java
@@ -19,6 +19,8 @@ import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 import com.amazonaws.services.dynamodbv2.model.ComparisonOperator;
 import com.amazonaws.services.dynamodbv2.model.Condition;
 import com.amazonaws.services.dynamodbv2.model.CreateTableRequest;
+import com.amazonaws.services.dynamodbv2.model.DeleteItemRequest;
+import com.amazonaws.services.dynamodbv2.model.DeleteItemResult;
 import com.amazonaws.services.dynamodbv2.model.KeySchemaElement;
 import com.amazonaws.services.dynamodbv2.model.KeyType;
 import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput;
@@ -29,11 +31,12 @@ import com.amazonaws.services.dynamodbv2.model.TableDescription;
 import com.huawei.iot.model.BusLine;
 import com.huawei.iot.model.Station;
 
-public class DBOperator{
+public class DBOperator {
 
 	private final static ProvisionedThroughput THRUPUT = new ProvisionedThroughput(2L, 1L);
 	protected static DynamoDB dynamo;
 	protected static AmazonDynamoDBClient client;
+
 	private static void init() {
 		client = new AmazonDynamoDBClient();
 		// 使用Singapore服务器
@@ -41,6 +44,7 @@ public class DBOperator{
 		client.setRegion(tokyo);
 		dynamo = new DynamoDB(client);
 	}
+
 	public static void createTable(String tblName, String hashKey) throws InterruptedException {
 		init();
 		// Table doesn't exist. Let's create it.
@@ -64,45 +68,45 @@ public class DBOperator{
 		return req;
 	}
 
-	public static void putItem(String tblName, List<BusLine> busLines){
+	public static void putItem(String tblName, List<BusLine> busLines) {
 		init();
 		Table table = dynamo.getTable(tblName);
 		List<Item> items = null;
-		if(DBConstants.TBL_BUSLINE.equals(tblName)){
+		if (DBConstants.TBL_BUSLINE.equals(tblName)) {
 			items = newBusLineItems(busLines);
-		}else if(DBConstants.TBL_STATION.equals(tblName)){
+		} else if (DBConstants.TBL_STATION.equals(tblName)) {
 			items = newStationItems(busLines);
 		}
-		for(Item item : items){
+		for (Item item : items) {
 			table.putItem(item);
 		}
-		
 	}
-	
-	public static PutItemOutcome updateItem(String tblName, Map<String, String> map){
+
+	public static PutItemOutcome updateItem(String tblName, Map<String, String> map) {
 		init();
 		Table table = dynamo.getTable(tblName);
 		Item item = null;
-		if(DBConstants.TBL_DEVICE_INSTANCE.equals(tblName)){
+		if (DBConstants.TBL_DEVICE_INSTANCE.equals(tblName)) {
 			item = newInstanceItems(map);
 		}
 		return table.putItem(item);
-		
+
 	}
 
 	private static Item newInstanceItems(Map<String, String> map) {
 		// TODO Auto-generated method stub
 		Item item = new Item().withString(DBConstants.HASH_KEY_NAME, map.get(DBConstants.DEVICE_ID))
 				.withString(DBConstants.POSITION_ID, map.get(DBConstants.POSITION_ID));
-				
+
 		return null;
 	}
+
 	private static List<Item> newStationItems(List<BusLine> busLines) {
 		// TODO Auto-generated method stub
 		List<Item> list = new ArrayList<Item>();
 		int stationHashID = 0;
-		for(BusLine bl : busLines){
-			for(Station station : bl.getStations()){
+		for (BusLine bl : busLines) {
+			for (Station station : bl.getStations()) {
 				Item item = new Item().withString(DBConstants.HASH_KEY_NAME, stationHashID + "")
 						.withString(DBConstants.STATION_ID, station.getId())
 						.withString(DBConstants.STATION_NAME, station.getName())
@@ -112,17 +116,18 @@ public class DBOperator{
 						.withString(DBConstants.STATION_SPE, station.getSpeed())
 						.withString(DBConstants.STATION_REMAIN_TIME, station.getRemainTime())
 						.withString(DBConstants.STATION_LINE_ID, bl.getLineNum());
-				stationHashID ++;
+				stationHashID++;
 				list.add(item);
 			}
 		}
-		
+
 		return list;
 	}
+
 	private static List<Item> newBusLineItems(List<BusLine> busLines) {
 		// TODO Auto-generated method stub
 		List<Item> list = new ArrayList<Item>();
-		for(BusLine bl : busLines){
+		for (BusLine bl : busLines) {
 			Item item = new Item().withString(DBConstants.HASH_KEY_NAME, bl.getLineNum())
 					.withString(DBConstants.LINE_START_TIME, bl.getStartTime())
 					.withString(DBConstants.LINE_END_TIME, bl.getEndTime())
@@ -130,41 +135,61 @@ public class DBOperator{
 					.withString(DBConstants.LINE_STATUS, DBConstants.STATUS_ENABLE);
 			list.add(item);
 		}
-		
+
 		return list;
 	}
-	public static GetItemOutcome getItem(String tblName, String hashKey){
+
+	public static GetItemOutcome getItem(String tblName, String hashKey) {
 		init();
 		Table table = dynamo.getTable(tblName);
-		GetItemOutcome outcome = table.getItemOutcome(new GetItemSpec()
-                .withPrimaryKey(DBConstants.HASH_KEY_NAME, hashKey));
-		
+		GetItemOutcome outcome = table
+				.getItemOutcome(new GetItemSpec().withPrimaryKey(DBConstants.HASH_KEY_NAME, hashKey));
+
 		return outcome;
 	}
-	public static ScanResult getAllItem(String tblName){
+
+	public static ScanResult getAllItem(String tblName) {
 		init();
 		ScanRequest scanRequest = new ScanRequest(tblName);
 		return client.scan(scanRequest);
 	}
-/*	private static Item newItem() {
-		Item item = new Item().withString(HASH_KEY_NAME, "foo")
-				.withBinary("binary", new byte[] { 1, 2, 3, 4 })
-				.withBinarySet("binarySet", new byte[] { 5, 6 }, new byte[] { 7, 8 })
-				.withBoolean("booleanTrue", true)
-				.withBoolean("booleanFalse", false).withInt("intAttr", 1234).withList("listAtr", "abc", "123")
-				.withMap("mapAttr", new ValueMap().withString("key1", "value1").withInt("key2", 999))
-				.withNull("nullAttr").withNumber("numberAttr", 999.1234).withString("stringAttr", "bla")
-				.withStringSet("stringSetAttr", "da", "di", "foo", "bar", "bazz");
-		return item;
+
+	public static void delItems(String tblName) {
+		init();
+		for(int i=0; i<4;i++){
+			HashMap<String, Condition> scanFilter = new HashMap<String, Condition>();
+	        Condition condition = new Condition()
+	            .withComparisonOperator(ComparisonOperator.CONTAINS.toString())
+	            .withAttributeValueList(new AttributeValue().withS(DIConstants.DATA_STREAM_ID+"_"+i));
+	        scanFilter.put(DIConstants.DATA_STREAM_ID, condition);
+	        ScanRequest scanRequest = new ScanRequest(tblName).withScanFilter(scanFilter);
+	        
+			List<Map<String, AttributeValue>> scanList = client.scan(scanRequest).getItems();
+			for (Map<String, AttributeValue> attr : scanList) {
+				String id = attr.get(DIConstants.DATA_STREAM_ID).getS();
+				Map<String, AttributeValue> key = new HashMap<String, AttributeValue>();
+				key.put("DataStreamID", new AttributeValue().withS(id));
+
+				DeleteItemRequest deleteItemRequest = new DeleteItemRequest(tblName, key);
+				DeleteItemResult deleteItemResult = client.deleteItem(deleteItemRequest);
+			}
+		}
+		
 	}
-	public static String simpleQuery() {
-        Table table = dynamo.getTable(TABLE_NAME);
-        ItemCollection<?> col = table.query(HASH_KEY_NAME, "foo");
-        int count = 0; 
-        for (Item item: col) {
-            System.out.println(item);
-            count++;
-        }
-        return col.toString();
-    }*/
+	/*
+	 * private static Item newItem() { Item item = new
+	 * Item().withString(HASH_KEY_NAME, "foo") .withBinary("binary", new byte[]
+	 * { 1, 2, 3, 4 }) .withBinarySet("binarySet", new byte[] { 5, 6 }, new
+	 * byte[] { 7, 8 }) .withBoolean("booleanTrue", true)
+	 * .withBoolean("booleanFalse", false).withInt("intAttr",
+	 * 1234).withList("listAtr", "abc", "123") .withMap("mapAttr", new
+	 * ValueMap().withString("key1", "value1").withInt("key2", 999))
+	 * .withNull("nullAttr").withNumber("numberAttr",
+	 * 999.1234).withString("stringAttr", "bla") .withStringSet("stringSetAttr",
+	 * "da", "di", "foo", "bar", "bazz"); return item; } public static String
+	 * simpleQuery() { Table table = dynamo.getTable(TABLE_NAME);
+	 * ItemCollection<?> col = table.query(HASH_KEY_NAME, "foo"); int count = 0;
+	 * for (Item item: col) { System.out.println(item); count++; } return
+	 * col.toString(); }
+	 */
 }
